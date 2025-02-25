2def7e6bb442ac8fd38d8aad65bb87f001ad8dc3
Gani2004 <132196673+Gani2004@users.noreply.github.com>
2025-02-25 12:58:07 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # User Model
 # -------------------------
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
